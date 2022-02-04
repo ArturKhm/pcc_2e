1615691c7f75fe3261122713827121ecb676fb5f
arturhamidullin <Art2486GitHub>
2022-02-04 13:18:46 +0300

    update

--- chapter_04/squares.py
+++ chapter_04/squares.py
@@ -1,2 +1,3 @@
 squares = [value**2 for value in range(1, 11)]
 print(squares)
+print(squares[int(len(squares)/2):-1])
--- chapter_05/banned_users.py
+++ chapter_05/banned_users.py
@@ -1,5 +1,6 @@
 banned_users = ['andrew', 'carolina', 'david']
-user = 'marie'
+# user = 'marie'
+user = 'andrew'
 
 if user not in banned_users:
     print(f"{user.title()}, you can post a response if you wish.")
