6d927c6ef78fdcb4d0056c9d164fe78768bd64e6
arturhamidullin <Art2486GitHub>
2022-02-04 11:39:50 +0300

    update

--- chapter_03/dinner.py
+++ chapter_03/dinner.py
@@ -13,3 +13,9 @@ print(guests.index('Masha'))
 guests.append('Galina')
 for guest in guests:
     print(f"Hi, {guest.title()}! You are invited.")
+print('Only two seats are available')
+while int(len(guests)) > 2:
+    last_value = guests.pop(-1)
+    print(f"You are out, {last_value}")
+for guest in guests:
+    print(f"Hi, {guest.title()}! You are invited.")
