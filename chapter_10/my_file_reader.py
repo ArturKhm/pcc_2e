59259e1176466fbc44ad3f54e4e567fc29ae5276
arturhamidullin <Art2486GitHub>
2022-02-07 15:07:38 +0300

    update

--- chapter_10/my_file_reader.py
+++ chapter_10/my_file_reader.py
@@ -1,8 +1,13 @@
-filename = 'learning_python.txt'
-with open(filename) as file_object:
-    # for line in file_object:
-    #     print(line)
-    # print(file_object.read())
-    lines = file_object.readlines()
-for line in lines:
-    print(line.strip().replace('Python', 'C'))
+filenames = ['cats.txt', 'dogs.txt']
+for filename in filenames:
+    try:
+        with open(filename) as file_object:
+            # for line in file_object:
+            #     print(line)
+            # print(file_object.read())
+            lines = file_object.readlines()
+            for line in lines:
+                print(line.strip())
+    except FileNotFoundError:
+        # print(f"{filename} doesn't exist")
+        pass
