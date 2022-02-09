be87cc555a4625b4a59ec711cfa4807f93c57c48
arturhamidullin <Art2486GitHub>
2022-02-09 18:32:56 +0300

    update

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -5,6 +5,7 @@ from ship import Ship
 
 class Scoreboard:
     """A class to report scoring information."""
+
     def __init__(self, ai_game):
         """Initialize scorekeeping attributes."""
         self.ai_game = ai_game
@@ -12,6 +13,7 @@ class Scoreboard:
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
         self.stats = ai_game.stats
+
         # Font settings for scoring information.
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)
